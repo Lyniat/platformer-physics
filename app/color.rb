517814666eea3f587c1b14d8e4437018454b84b5
Laurin Muth <l.muth@lyniat.games>
2022-03-09 22:28:16 +0100

    Fix wrong colors

--- app/color.rb
+++ app/color.rb
@@ -10,8 +10,8 @@ class Color
 
   WHITE = Color.new(255, 255, 255)
   RED = Color.new(255, 0, 0)
-  BLUE = Color.new(0, 255, 0)
-  GREEN = Color.new(0, 0, 255)
+  BLUE = Color.new(0, 0, 255)
+  GREEN = Color.new(0, 255, 0)
   BLACK = Color.new(0, 0, 0)
   YELLOW = Color.new(255, 255, 0)
 end
